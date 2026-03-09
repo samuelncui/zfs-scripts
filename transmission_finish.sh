81a93287ff072a0e732a2d78f87d8ae609c77c7e
Samuel N Cui <cuijingning@bytedance.com>
2026-03-09 19:47:00 +0800

    feat: add set test

--- transmission_finish.sh
+++ transmission_finish.sh
@@ -15,6 +15,19 @@ if [[ -f "$ENV_FILE" ]]; then
   set +a
 fi
 
+if [ -z "${TR_TORRENT_DIR:-}" ]; then
+    echo "TR_TORRENT_DIR not set"
+    exit 1
+fi
+if [ -z "${TR_TORRENT_NAME:-}" ]; then
+    echo "TR_TORRENT_NAME not set"
+    exit 1
+fi
+if [ -z "${TARGET_DIR:-}" ]; then
+    echo "TARGET_DIR not set"
+    exit 1
+fi
+
 exec python3 "${SCRIPT_DIR}/transmission_finish.py" \
   --source "${TR_TORRENT_DIR}/${TR_TORRENT_NAME}" \
   --target "${TARGET_DIR}/${TR_TORRENT_NAME}"
